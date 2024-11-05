f5f5b4208c3b800ec5159c132658e33730a19b4e
Daniil Krasnov <d.krasnov@hh.ru>
2024-11-05 10:50:18 +0300

    init

---
+++ src/exercise/Main.java
@@ -0,0 +1,34 @@
+package exercise;
+
+import calculator.Calculator;
+import calculator.Operation;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Calculator calculator = new Calculator();
+    Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
+
+    System.out.println("Добро пожаловать в игру на сложение и вычитание!");
+
+    int points = 0;
+    while (true) {
+      int a = random.nextInt(1, 100);
+      int b = random.nextInt(1, 100);
+      Operation operation = List.of(Operation.PLUS, Operation.MINUS).get(random.nextInt(2));
+      int expected = calculator.calculate(a, operation, b);
+
+      System.out.println("Сколько будет " + a + operation + b + "?");
+      int answer = scanner.nextInt();
+
+      if (answer != expected) {
+        System.out.println("Вы набрали " + points);
+        System.exit(0);
+      }
+      points++;
+    }
+  }
+}
